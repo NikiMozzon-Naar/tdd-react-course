0415dd28e120c9f69a9fa68bb4c8563846945f20
Niki-Mozzon <155958538+NikiMozzon-Naar@users.noreply.github.com>
2026-03-17 09:55:15 +0100

    Turn to Typescript

--- 01-starting-project/src/index.tsx
+++ 01-starting-project/src/index.tsx
@@ -1,7 +1,9 @@
 import ReactDOM from "react-dom/client";
 
-import App from "./App.jsx";
+import App from "./App";
 import "./index.css";
 
 const entryPoint = document.getElementById("root");
-ReactDOM.createRoot(entryPoint).render(<App />);
+if (entryPoint) {
+  ReactDOM.createRoot(entryPoint).render(<App />);
+}
